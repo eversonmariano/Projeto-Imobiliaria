37885963469567f4b319854145cd936fa83ceb83
eversonmariano <mariano.computacao@gmail.com>
2020-11-22 23:45:14 -0300

    Projeto Imobiliaria Usando JAVAFX e o MySQL.

---
+++ Projetoimob/src/projetoimob/view/FXMLTelaPrincipalController.java
@@ -0,0 +1,14 @@
+package projetoimob.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FXMLTelaPrincipalController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+}
